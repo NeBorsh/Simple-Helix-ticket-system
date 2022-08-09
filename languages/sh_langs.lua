1b74765b16d02bf9cdbe469734190fa95001293f
Borisenkoff <85323386+Borisenkoff14@users.noreply.github.com>
2022-08-09 18:42:52 +0300

    Create sh_langs.lua
    Made a translation of the plugin

---
+++ languages/sh_langs.lua
@@ -0,0 +1,40 @@
+--[[
+⊱⋅ ─── Eng ─── ⋅⊰
+]]--
+
+ticketdisc = 'Ticket description'
+
+ticketname = 'Ticket Names'
+
+Createmenu = 'Document creation menu'
+
+gosbl = 'You are not a government employee'
+
+createticket = 'Create a ticket'
+
+--[[
+⊱⋅ ─── Rus ─── ⋅⊰
+]]--
+
+--[[
+ticketdisc = 'Описание тикета'
+
+ticketname = 'Названия тикета'
+
+Createmenu = 'Меню создания документа'
+
+gosbl = 'Вы не государственный служащий'
+
+createticket = 'Создать тикет'
+]]--
+
+--[[
+⊱⋅ ─── Инструкция : Instruction ─── ⋅⊰
+]]--
+
+--[[
+
+[ 1 ][ eng ] Remove / add  comment to select language
+[ 2 ][ rus ] Уберите / добавте коментарий, для выбора языка
+
+]]--
